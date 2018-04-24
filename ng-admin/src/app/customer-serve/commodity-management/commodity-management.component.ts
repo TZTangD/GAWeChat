a7a6bf3128f2db76884b8f05de86a01558250669
shiguanWXX <1358110409@qq.com>
2018-04-24 16:35:50 +0800

    客户服务

---
+++ ng-admin/src/app/customer-serve/commodity-management/commodity-management.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'commodity-management',
+    templateUrl: 'commodity-management.component.html',
+})
+export class CommodityManagementComponent {
+
+}
--- ng-admin/src/app/customer-serve/customer-serve.module.ts
+++ ng-admin/src/app/customer-serve/customer-serve.module.ts
@@ -4,6 +4,13 @@ import { AppRouteGuard } from "@shared/auth/auth-route-guard";
 import { CustomerServeRoutingModule } from "./customer-serve-routing.module";
 import { SharedModule } from "@shared/shared.module";
 import { LayoutModule } from "../layout/layout.module";
+import { RetailCustomerComponent } from "./retail-customer/retail-customer.component";
+import { RetailCustomerDetailComponent } from "./retail-customer/retail-customer-detail/retail-customer-detail.component";
+import { StoreManagementComponent } from "./store-management/store-management.component";
+import { EmployeeModalComponent } from "./retail-customer/retail-customer-detail/employee-modal/employee-modal.component";
+import { CommodityManagementComponent } from "./commodity-management/commodity-management.component";
+import { SourceGoodsLevelComponent } from "./source-goods-level/source-goods-level.component";
+import { AccountManagementComponent } from "./account-management/account-management.component";
 
 @NgModule({
     imports: [
@@ -12,7 +19,13 @@ import { LayoutModule } from "../layout/layout.module";
         CustomerServeRoutingModule
     ],
     declarations: [
-
+        RetailCustomerComponent,
+        RetailCustomerDetailComponent,
+        EmployeeModalComponent,
+        StoreManagementComponent,
+        CommodityManagementComponent,
+        SourceGoodsLevelComponent,
+        AccountManagementComponent,
     ],
     providers: [
         AppRouteGuard  
