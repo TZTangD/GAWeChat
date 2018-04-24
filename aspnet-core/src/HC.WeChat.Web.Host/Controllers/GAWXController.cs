3fbdaf503161980722bb555c64313d965d91a14c
Donald <tangdezou@qq.com>
2018-04-24 18:17:14 +0800

    架构完善

---
+++ aspnet-core/src/HC.WeChat.Web.Host/Controllers/GAWXController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HC.WeChat.Activities;
+using HC.WeChat.Authorization.WeChatOAuth;
+using HC.WeChat.Configuration;
+using HC.WeChat.Controllers;
+using HC.WeChat.Models.WeChat;
+using HC.WeChat.WechatAppConfigs;
+using HC.WeChat.WeChatUsers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace HC.WeChat.Web.Host.Controllers
+{
+    public class GAWXController : WeChatWebControllerBase
+    {
+        IWeChatOAuthAppService _weChatOAuthAppService;
+        IWeChatUserAppService _weChatUserAppService;
+        private readonly IConfigurationRoot _appConfiguration;
+        private WeChatTenantSetting _settings;
+        private int? tenantId;
+
+        private string UserOpenId
+        {
+            get
+            {
+                if (HttpContext.Session.GetString("UserOpenId") == null)
+                {
+                    return string.Empty;
+                }
+                return HttpContext.Session.GetString("UserOpenId");
+            }
+            set
+            {
+                value = value ?? string.Empty;
+                HttpContext.Session.SetString("UserOpenId", value);
+            }
+        }
+
+        protected override int? GetTenantId()
+        {
+            return tenantId;
+        }
+
+        public GAWXController(IWechatAppConfigAppService wechatAppConfigAppService,
+          IOptions<WeChatTenantSetting> settings,
+          IWeChatOAuthAppService weChatOAuthAppService,
+          IWeChatUserAppService weChatUserAppService,
+          IActivityAppService activityAppService,
+          IHostingEnvironment env) : base(wechatAppConfigAppService)
+        {
+            _settings = settings.Value;
+            tenantId = _settings.GuangAn;
+            InitAppConfigSetting();
+
+            _weChatOAuthAppService = weChatOAuthAppService;
+            _weChatUserAppService = weChatUserAppService;
+            _weChatOAuthAppService.WechatAppConfig = WechatAppConfig;//注入配置
+            _appConfiguration = env.GetAppConfiguration();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ng-wechat/src/app/wechat/components/page/page.component.ts
+++ ng-wechat/src/app/wechat/components/page/page.component.ts
@@ -3,14 +3,16 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
 @Component({
     selector: 'Page',
     template: `
-    <div class="page__hd">
+    <div class="page__hd" *ngIf="showTitle" >
         <h1 class="page__title" [innerHTML]="title"></h1>
         <p class="page__desc" [innerHTML]="subTitle"></p>
     </div>
     <div class="page__bd" [ngClass]="{'page__bd_spacing': spacing}"><ng-content></ng-content></div>
     <div class="page__ft" [ngClass]="{'j_bottom': ftBottom}" *ngIf="!noBottom">
-        <a href="#" routerLink="/"><img src="./assets/images/icon_footer.png"></a>
-        <div class="weui-footer">
+    <div class="weui-footer">
+        <p class="weui-footer__links" *ngIf="showBindStaff">
+        <a href="javascript:void(0);" class="weui-footer__link">内部员工绑定</a>
+        </p>
         <p class="weui-footer__text">广安烟草 | 渠江烟语</p>
     </div>
         <ng-content select="[footer]"></ng-content>
@@ -28,4 +30,6 @@ export class PageComponent {
     @Input() spacing: boolean = true;
     @Input() ftBottom: boolean = false;
     @Input() noBottom: boolean = false;
+    @Input() showTitle: boolean = true;
+    @Input() showBindStaff: boolean = false;
 }
