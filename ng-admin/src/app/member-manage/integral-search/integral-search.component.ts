812c68915aa73f7b62de4ece16c280c51e9f0ab5
shiguanWXX <1358110409@qq.com>
2018-04-24 16:57:10 +0800

    会员管理

---
+++ ng-admin/src/app/member-manage/integral-search/integral-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'integral-search',
+    templateUrl: 'integral-search.component.html',
+})
+export class IntegralSearchComponent {
+
+}
--- ng-admin/src/app/member-manage/member-manage-routing.module.ts
+++ ng-admin/src/app/member-manage/member-manage-routing.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { LayoutDefaultComponent } from "../layout/default/default.component";
+import { MemberManagementComponent } from "./member-management/member-management.component";
+import { AppRouteGuard } from "@shared/auth/auth-route-guard";
+import { IntegralSearchComponent } from "./integral-search/integral-search.component";
+import { MemberSettingComponent } from "./member-setting/member-setting.component";
 
 const routes: Routes = [{
     path: '',
     component: LayoutDefaultComponent,
     children: [
         { path: '', redirectTo: 'index', pathMatch: 'full' },
-        // { path: 'index', component: IndexComponent, data: { translate: 'index' }, canActivate: [AppRouteGuard] },
+        { path: 'member-management', component: MemberManagementComponent, data: { translate: 'member-management', permission: 'Pages' }, canActivate: [AppRouteGuard] },
+        { path: 'integral-search', component: IntegralSearchComponent, data: { translate: 'integral-search', permission: 'Pages' }, canActivate: [AppRouteGuard] },
+        { path: 'member-setting', component: MemberSettingComponent, data: { translate: 'member-setting', permission: 'Pages' }, canActivate: [AppRouteGuard] },
+        
     ]
   }];
   @NgModule({
