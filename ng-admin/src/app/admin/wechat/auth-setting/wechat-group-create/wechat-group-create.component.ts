f4d3e1663b708b550a6a41649b1bf314195adfe9
shiguanWXX <1358110409@qq.com>
2018-05-09 17:23:17 +0800

    分组

--- ng-admin/src/app/admin/wechat/auth-setting/wechat-group-create/wechat-group-create.component.ts
+++ ng-admin/src/app/admin/wechat/auth-setting/wechat-group-create/wechat-group-create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Injector } from '@angular/core';
 import { AppComponentBase } from '@shared/app-component-base';
 import { WeChatGroupServiceProxy } from '@shared/service-proxies/wechat-service';
 import { WeChatGroup } from '@shared/entity/wechat';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     moduleId: module.id,
@@ -10,17 +11,33 @@ import { WeChatGroup } from '@shared/entity/wechat';
     styleUrls: ['wechat-group-create.component.scss']
 })
 export class WechatGroupCreateComponent extends AppComponentBase implements OnInit {
-    cdalVisible=false;
-    isConfirmLoading=false;
-    wechatGroup:WeChatGroup=new WeChatGroup(); 
-    constructor(injector: Injector, private wechatGroupService: WeChatGroupServiceProxy) {
+    cdalVisible = false;
+    isConfirmLoading = false;
+    wechatGroup: WeChatGroup = new WeChatGroup();
+    formc: FormGroup;
+    constructor(injector: Injector, private wechatGroupService: WeChatGroupServiceProxy, private fb: FormBuilder) {
         super(injector);
     }
     ngOnInit(): void {
-
+        this.formc = this.fb.group({
+            typeCode: [null, Validators.compose([Validators.required])],
+            tagName: [null, Validators.compose([Validators.required])],
+        });
     }
     show() {
-this.cdalVisible=true;
+        this.reset();
+        this.cdalVisible = true;
+    }
+    chandleCancel=(e)=>{
 
+    }
+    reset(e?): void {
+        if (e) {
+            e.preventDefault();
+        }
+        this.formc.reset();
+        for (const key in this.formc.controls) {
+            this.formc.controls[key].markAsPristine();
+        }
     }
 }
