30e478f6e17bd2b04fea76e5f5de3a6f4f86f84a
YangFan17 <173838362@qq.com>
2018-05-28 18:32:46 +0800

    编辑器代码莫事

--- ng-admin/src/app/admin/marketting/activity/activity-detail/activity-detail.component.ts
+++ ng-admin/src/app/admin/marketting/activity/activity-detail/activity-detail.component.ts
@@ -32,12 +32,20 @@ export class ActivityDetailComponent extends AppComponentBase implements OnInit 
         height: 550,
         plugins: [
             'advlist autolink autosave link image lists charmap print preview hr anchor pagebreak',
-            'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
-            'table contextmenu directionality emoticons template textcolor paste fullpage textcolor colorpicker textpattern'
+            'table searchreplace wordcount visualblocks visualchars fullscreen insertdatetime media nonbreaking code',
+            'contextmenu directionality emoticons template textcolor paste fullpage textcolor colorpicker textpattern'
         ],
         toolbar1: 'bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | styleselect formatselect fontselect fontsizeselect',
-        toolbar2: 'bullist numlist | outdent indent blockquote | undo redo | image code| insertdatetime preview | forecolor backcolor',
-        toolbar3: 'table | hr removeformat | charmap | ltr rtl | visualchars visualblocks template pagebreak restoredraft',
+        toolbar2: 'bullist numlist | outdent indent blockquote | undo redo | image | insertdatetime preview | forecolor backcolor',
+        toolbar3: 'code | table hr removeformat | charmap | ltr rtl | visualchars visualblocks template pagebreak restoredraft',
+        // plugins: [
+        //     'advlist autolink autosave link image lists charmap print preview hr anchor pagebreak',
+        //     'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+        //     'table contextmenu directionality emoticons template textcolor paste fullpage textcolor colorpicker textpattern'
+        // ],
+        // toolbar1: 'newdocument fullpage | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | styleselect formatselect fontselect fontsizeselect',
+        // toolbar2: 'cut copy paste | searchreplace | bullist numlist | outdent indent blockquote | undo redo | link unlink anchor image media code | insertdatetime preview | forecolor backcolor',
+        // toolbar3: 'table | hr removeformat | subscript superscript | charmap emoticons | print fullscreen | ltr rtl | visualchars visualblocks nonbreaking template pagebreak restoredraft',
         // content_css: [
         //     '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
         //     '//www.tinymce.com/css/codepen.min.css'],
