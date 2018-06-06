ae6ed91731f276a94b988b2b70b9b51c19a6604c
DonaldTdz <tangdezou@qq.com>
2018-06-06 23:20:12 +0800

    修改缩放级别

--- ng-wechat/src/app/wechat/personal-center/shop/shop-add/shop-add.component.ts
+++ ng-wechat/src/app/wechat/personal-center/shop/shop-add/shop-add.component.ts
@@ -241,7 +241,7 @@ export class ShopAddComponent extends AppComponentBase implements OnInit {
             longitude: (this.qqLongitude ? this.qqLongitude : this.longitude), // 经度，浮点数，范围为180 ~ -180。
             name: this.res.name, // 位置名
             address: this.res.address, // 地址详情说明
-            scale: 15, // 地图缩放级别,整形值,范围从1~28。默认为最大
+            scale: 16, // 地图缩放级别,整形值,范围从1~28。默认为最大
             infoUrl: this.hostUrl + '/gawechat/index.html#/shops/shop' // 在查看位置界面底部显示的超链接,可点击跳转
         });
     }
