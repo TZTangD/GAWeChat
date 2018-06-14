5ab683cbe5dab20372ea63522211a2917ea17884
YangFan17 <173838362@qq.com>
2018-06-14 18:43:07 +0800

    提交

--- aspnet-core/src/Abp.WeChat.Senparc/Senparc/MessageHandlers/AbpMessageHandler/AbpMessageInfo.cs
+++ aspnet-core/src/Abp.WeChat.Senparc/Senparc/MessageHandlers/AbpMessageHandler/AbpMessageInfo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Senparc.Weixin.MP.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -11,7 +12,7 @@ namespace Abp.WeChat.Senparc.MessageHandlers
         public virtual string Title { get; set; }
         public virtual string Desc { get; set; }
 
-        public virtual Dictionary<string, AbpMessageInfo> KeyWordsPic { get; set; }
+        public virtual Dictionary<string, Article> KeyWordsPic { get; set; }
         public virtual Dictionary<string, string> KeyWords { get; set; }
     }
 }
